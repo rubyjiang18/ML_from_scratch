2606b939d627ccc9f54cd6d2f335f77354f5dfd1
Runbo Jiang <runboj@andrew.cmu.edu>
2023-05-15 14:39:40 -0400

    Update

---
+++ LinearNN/torch.py
@@ -0,0 +1,67 @@
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+from torch import nn
+from torch.nn import functional as F
+from torch.utils import data
+from torchvision import transforms
+
+nn_Module = nn.Module
+
+
+#################   WARNING   ################
+# The below part is generated automatically through:
+#    d2lbook build lib
+# Don't edit it directly
+
+import collections
+import hashlib
+import inspect
+import math
+import os
+import random
+import re
+import shutil
+import sys
+import tarfile
+import time
+import zipfile
+from collections import defaultdict
+import pandas as pd
+import requests
+from IPython import display
+from matplotlib import pyplot as plt
+from matplotlib_inline import backend_inline
+
+d2l = sys.modules[__name__]
+
+import gym
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+from scipy.spatial import distance_matrix
+from torch import nn
+from torch.nn import functional as F
+from torchvision import transforms
+
+
+class HyperParameters:
+    """The base class of hyperparameters."""
+    def save_hyperparameters(self, ignore=[]):
+        """Defined in :numref:`sec_oo-design`"""
+        raise NotImplemented
+
+    def save_hyperparameters(self, ignore=[]):
+        """Save function arguments into class attributes.
+    
+        Defined in :numref:`sec_utils`"""
+        frame = inspect.currentframe().f_back
+        _, _, _, local_vars = inspect.getargvalues(frame)
+        self.hparams = {k:v for k, v in local_vars.items()
+                        if k not in set(ignore+['self']) and not k.startswith('_')}
+        for k, v in self.hparams.items():
+            setattr(self, k, v)
+
+
---
+++ LinearNN/trainer.py
@@ -0,0 +1,4 @@
+from d2l import torch as d2l
+
+class Trainer(d2l.HyperParameter):
+    """The base class for training models with data"""
